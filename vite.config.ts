e075d574126c0e09ca47599e5ac24c20993f24b9
Atlantix357 <taurisgu@gmail.com>
2025-03-06 16:41:19 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  server: {
+    port: 3501,
+    host: true,
+    open: true,
+    strictPort: false
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false,
+    minify: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          dexie: ['dexie']
+        }
+      }
+    }
+  },
+  base: '/BookManager/' // Set base path to /BookManager/
+})
